19ebe30f443ae5118a33a24d74968d120b5e3a35
Anton Maurovic <anton@foogle.com>
2023-10-29 13:16:24 +1030

    Starting to instantiate raybox-zero project via submodule

--- README.md
+++ README.md
@@ -1,12 +1,27 @@
-# Caravel User Project
+# raybox-zero-caravel
 
-[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0) [![UPRJ_CI](https://github.com/efabless/caravel_project_example/actions/workflows/user_project_ci.yml/badge.svg)](https://github.com/efabless/caravel_project_example/actions/workflows/user_project_ci.yml) [![Caravel Build](https://github.com/efabless/caravel_project_example/actions/workflows/caravel_build.yml/badge.svg)](https://github.com/efabless/caravel_project_example/actions/workflows/caravel_build.yml)
+[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
 
-| :exclamation: Important Note            |
-|-----------------------------------------|
+This is a clone of [caravel_user_project] (at the time of writing, based on tag [mpw-8c] per the [Zero to ASIC Course]).
 
-## Please fill in your project documentation in this README.md file 
+The [ew branch] is where it began life, and is probably still the default.
+
+This repo wraps [raybox-zero] (namely its `ew` branch too).
+
+**This design implements a simple hardware ray caster** and while it is hardened as its own GDS macro and placed inside the Caravel `user_project_wrapper`, it is (hopefully) going to be part of a shared chipIgnite shuttle project. Given this, it has only 9 dedicated Caravel IO pads, but also makes extensive use of Logic Analyzer pins.
+
+**More to come!**
+
+
+[caravel_user_project]: https://github.com/efabless/caravel_user_project
+[mpw-8c]: https://github.com/efabless/caravel_user_project/tree/mpw-8c
+[Zero to ASIC Course]: https://zerotoasiccourse.com/
+[ew branch]: https://github.com/algofoogle/raybox-zero-caravel/tree/ew
+[raybox zero]: https://github.com/algofoogle/raybox-zero/tree/ew
+
+## Old notes/guides from eFabless
 
 Refer to [README](docs/source/index.rst#section-quickstart) for a quickstart of how to use caravel_user_project
 
 Refer to [README](docs/source/index.rst) for this sample project documentation. 
+
--- verilog/rtl/user_project_wrapper.v
+++ verilog/rtl/user_project_wrapper.v
@@ -82,41 +82,89 @@ module user_project_wrapper #(
 /* User project is instantiated  here   */
 /*--------------------------------------*/
 
-user_proj_example mprj (
-`ifdef USE_POWER_PINS
-	.vccd1(vccd1),	// User area 1 1.8V power
-	.vssd1(vssd1),	// User area 1 digital ground
-`endif
+    //// BEGIN: INSTANTIATION OF ANTON'S DESIGN (top_ew_algofoogle) (SNIPPET1_NoShare) ---------------------
 
-    .wb_clk_i(wb_clk_i),
-    .wb_rst_i(wb_rst_i),
+    // This snippet comes from here:
+    // https://github.com/algofoogle/raybox-zero/blob/ew/src/rtl/ew_caravel_snippets/SNIPPET1_NoShare.v
 
-    // MGMT SoC Wishbone Slave
+    // Anton's assigned pads are IO[26:18]...
+    // These allow easy renumbering of those pads, if necessary.
+    assign anton_io_in = io_in[26:18];      // Map the 'in' side of our 9 pads.
+    assign io_out[26:18] = anton_io_out;    // Map the 'out' side of our 9 pads.
+    assign io_oeb[26:18] = anton_io_oeb;    // Map the IO OEBs for our pads.
+    // Convenience mapping of LA[114:64] to anton_la_in[50:0]. All are INPUTS INTO our module:
+    wire [50:0] anton_la_in   = la_data_in[114:64];
+    wire [50:0] anton_la_oenb =    la_oenb[114:64]; // SoC should configure these all as its outputs (i.e. inputs to our design).
 
-    .wbs_cyc_i(wbs_cyc_i),
-    .wbs_stb_i(wbs_stb_i),
-    .wbs_we_i(wbs_we_i),
-    .wbs_sel_i(wbs_sel_i),
-    .wbs_adr_i(wbs_adr_i),
-    .wbs_dat_i(wbs_dat_i),
-    .wbs_ack_o(wbs_ack_o),
-    .wbs_dat_o(wbs_dat_o),
+    // Abtractions between Anton's top design and the above pads.
+    wire [8:0]  anton_io_in;                // Design-driven: 'In' side of abtracted pads. Not all INs are used.
+    wire [8:0]  anton_io_out;               // Design-driven: 'Out' side of abstracted pads. Not all OUTs are used.
+    wire [8:0]  anton_io_oeb;               // Design-driven: Direction control for each abstracted pad.
+    // Splicing the various signals together into their respective abstracted pads:
+    wire        anton_tex_oeb0;             // Design-driven: Controls dir of one specific IO pad (Texture QSPI io[0]).
+    wire [5:0]  anton_gpout;                // Design-driven: We splice 2 LSB into anton_io_out, discard upper 4.
+    wire [15:0] a0s, a1s;                   // Low and high signals from our design that we can use to mix constants.
+    wire [8:0]  anton_io_oeb = {a1s[1:0], a0s[1:0], anton_tex_oeb0, a0s[5:2]}; // 1100t0000 where 't' is anton_tex_oeb0.
+    assign      anton_io_out[6:5] = anton_gpout[1:0]; // Only use lower 2 (of 6) 'gpout's, plug them into the middle of Anton's OUTPUT pads.
+    wire [3:0]  anton_tex_in = {anton_la_in[50], anton_io_in[8], anton_io_in[7], anton_io_in[4]};
 
-    // Logic Analyzer
 
-    .la_data_in(la_data_in),
-    .la_data_out(la_data_out),
-    .la_oenb (la_oenb),
+    top_ew_algofoogle top_ew_algofoogle(
+    `ifdef USE_POWER_PINS
+    .vccd1(vccd1),        // User area 1 1.8V power
+    .vssd1(vssd1),        // User area 1 digital ground
+    `endif
 
-    // IO Pads
+        .i_clk                  (user_clock2),
+        .i_la_invalid           (anton_la_oenb[0]), // Check any one of our LA's OENBs. Should be 0 (i.e. driven by SoC) if valid.
+        .i_reset_lock_a         (anton_la_in[0]), // Hold design in reset if equal (both 0 or both 1)
+        .i_reset_lock_b         (anton_la_in[1]), // Hold design in reset if equal (both 0 or both 1)
 
-    .io_in (io_in),
-    .io_out(io_out),
-    .io_oeb(io_oeb),
+        .zeros                  (a0s),  // A source of 16 constant '0' signals.
+        .ones                   (a1s),  // A source of 16 constant '1' signals.
 
-    // IRQ
-    .irq(user_irq)
-);
+        .o_hsync                (anton_io_out[0]),
+        .o_vsync                (anton_io_out[1]),
+        //.o_rgb([23:0]) not used, except to feed DAC.
+
+        .o_tex_csb              (anton_io_out[2]),
+        .o_tex_sclk             (anton_io_out[3]),
+
+        .o_tex_oeb0             (anton_tex_oeb0), // My only bidirectional pad.
+        .o_tex_out0             (anton_io_out[4]),
+        .i_tex_in               (anton_tex_in),
+
+        .o_gpout                (anton_gpout), //NOTE: Lower 2 bits are used, upper 4 are not.
+
+        .i_vec_csb              (anton_la_in[2]),
+        .i_vec_sclk             (anton_la_in[3]),
+        .i_vec_mosi             (anton_la_in[4]),
+
+        .i_gpout0_sel           (anton_la_in[10:5]),
+
+        .i_debug_vec_overlay    (anton_la_in[11]),
+
+        .i_reg_csb              (anton_la_in[12]),
+        .i_reg_sclk             (anton_la_in[13]),
+        .i_reg_mosi             (anton_la_in[14]),
+
+        .i_gpout1_sel           (anton_la_in[20:15]),
+        .i_gpout2_sel           (anton_la_in[26:21]),
+
+        .i_debug_trace_overlay  (anton_la_in[27]),
+
+        .i_gpout3_sel           (anton_la_in[33:28]),
+
+        .i_debug_map_overlay    (anton_la_in[34]),
+
+        .i_gpout4_sel           (anton_la_in[40:35]),
+        .i_gpout5_sel           (anton_la_in[46:41]),
+
+        .i_mode                 (anton_la_in[49:47])
+    );
+
+    //// END: INSTANTIATION OF ANTON'S DESIGN (top_ew_algofoogle) (SNIPPET1_NoShare) ---------------------
+
 
 endmodule	// user_project_wrapper
 
